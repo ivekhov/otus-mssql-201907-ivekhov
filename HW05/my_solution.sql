c22cdf2b17a982e14050a83925ea31816c1d6e51
ivekhov <ivekhov@gmail.com>
2019-09-22 22:51:35 +0300

    added basic part of homework 05

---
+++ HW05/my_solution.sql
@@ -0,0 +1,207 @@
+﻿----------------------------------------------------------------------------
+-- Домашнее задание Группировки и агрегатные функции
+-- 1. Посчитать среднюю цену товара, общую сумму продажи по месяцам
+----------------------------------------------------------------------------
+
+USE WideWorldImporters;
+
+SELECT 
+DATENAME (year, so.OrderDate) AS YearOfOrder
+,MONTH(so.OrderDate) AS MonthNum
+,DATENAME (month, so.OrderDate) AS MonthOfOrder
+,CONCAT(
+		DATENAME (year, so.OrderDate)
+		, ' '
+		, DATENAME (month, so.OrderDate) ) AS YearMonth
+,SUM(sol.Quantity * sol.UnitPrice) AS GrossSales
+,AVG(sol.UnitPrice) AS AveragePrice
+FROM Sales.OrderLines AS sol
+LEFT JOIN Sales.Orders AS so
+	ON sol.OrderID = so.OrderID
+GROUP BY 
+DATENAME (year, so.OrderDate) 
+, MONTH(so.OrderDate)
+,DATENAME (month, so.OrderDate)
+ORDER BY 
+DATENAME (year, so.OrderDate) ASC
+,MONTH(so.OrderDate) ASC
+,DATENAME (month, so.OrderDate) 
+;
+GO
+
+------------------------------------------------------------
+-- 1 Модификация запроса 1.2. добавлено поле продукта.
+------------------------------------------------------------
+USE WideWorldImporters;
+
+SELECT 
+DATENAME (year, so.OrderDate) AS YearOfOrder
+,MONTH(so.OrderDate) AS MonthNum
+,DATENAME (month, so.OrderDate) AS MonthOfOrder
+,CONCAT(
+		DATENAME (year, so.OrderDate)
+		, ' '
+		, DATENAME (month, so.OrderDate) ) AS YearMonth
+, sol.Description AS Product
+,SUM(sol.Quantity * sol.UnitPrice) AS GrossSales
+,AVG(sol.UnitPrice) AS AveragePrice
+FROM Sales.OrderLines AS sol
+LEFT JOIN Sales.Orders AS so
+	ON sol.OrderID = so.OrderID
+GROUP BY 
+DATENAME (year, so.OrderDate) 
+,MONTH(so.OrderDate)
+,DATENAME (month, so.OrderDate)
+,sol.Description 
+ORDER BY 
+DATENAME (year, so.OrderDate) ASC
+,MONTH(so.OrderDate) ASC
+,SUM(sol.Quantity * sol.UnitPrice) DESC
+;
+GO
+
+---------------------------------------------------------------------
+-- 2. Отобразить все месяцы, где общая сумма продаж превысила 10 000 
+---------------------------------------------------------------------
+
+USE WideWorldImporters;
+
+SELECT 
+DATENAME (year, so.OrderDate) AS YearOfOrder
+,MONTH(so.OrderDate) AS MonthNum
+,DATENAME (month, so.OrderDate) AS MonthOfOrder
+,CONCAT(
+		DATENAME (year, so.OrderDate)
+		, ' '
+		, DATENAME (month, so.OrderDate) ) AS YearMonth
+,SUM(sol.Quantity * sol.UnitPrice) AS GrossSales
+,AVG(sol.UnitPrice) AS AveragePrice
+FROM Sales.OrderLines AS sol
+LEFT JOIN Sales.Orders AS so
+	ON sol.OrderID = so.OrderID
+GROUP BY 
+DATENAME (year, so.OrderDate) 
+, MONTH(so.OrderDate)
+,DATENAME (month, so.OrderDate)
+HAVING SUM(sol.Quantity * sol.UnitPrice) > 10000
+ORDER BY 
+DATENAME (year, so.OrderDate) ASC
+,MONTH(so.OrderDate) ASC
+,DATENAME (month, so.OrderDate) 
+;
+GO
+
+------------------------------------------------------------
+-- 2 Модификация запроса 2.2: добавлено поле продукта в таблицу
+------------------------------------------------------------
+
+USE WideWorldImporters;
+
+SELECT 
+DATENAME (year, so.OrderDate) AS YearOfOrder
+,MONTH(so.OrderDate) AS MonthNum
+,DATENAME (month, so.OrderDate) AS MonthOfOrder
+,CONCAT(
+		DATENAME (year, so.OrderDate)
+		, ' '
+		, DATENAME (month, so.OrderDate) ) AS YearMonth
+, sol.Description AS Product
+,SUM(sol.Quantity * sol.UnitPrice) AS GrossSales
+,AVG(sol.UnitPrice) AS AveragePrice
+FROM Sales.OrderLines AS sol
+LEFT JOIN Sales.Orders AS so
+	ON sol.OrderID = so.OrderID
+GROUP BY 
+DATENAME (year, so.OrderDate) 
+,MONTH(so.OrderDate)
+,DATENAME (month, so.OrderDate)
+,sol.Description 
+HAVING SUM(sol.Quantity * sol.UnitPrice) > 10000
+ORDER BY 
+DATENAME (year, so.OrderDate) ASC
+,MONTH(so.OrderDate) ASC
+,SUM(sol.Quantity * sol.UnitPrice) DESC
+;
+GO
+
+---------------------------------------------------------------
+/*
+3. Вывести сумму продаж,  дату первой продажи и 
+количество проданного по месяцам, по товарам, 
+продажи которых менее 50 ед в месяц. 
+Группировка должна быть по году и месяцу.
+*/
+---------------------------------------------------------------
+
+USE WideWorldImporters;
+WITH FirstOrderDate AS 
+(
+	SELECT 
+		sol.Description AS Product
+		,MIN(so.OrderDate) AS FirstOrderDate
+	FROM Sales.OrderLines AS sol
+	LEFT JOIN Sales.Orders AS so
+		ON sol.OrderID = so.OrderID
+	GROUP BY sol.Description
+)
+SELECT 
+	DATENAME (year, so.OrderDate) AS YearOfOrder
+	,MONTH(so.OrderDate) AS MonthNum
+	,DATENAME (month, so.OrderDate) AS MonthOfOrder
+	,CONCAT(
+			DATENAME (year, so.OrderDate)
+			, ' '
+			, DATENAME (month, so.OrderDate) ) AS YearMonth
+	,sol.Description AS Product
+	,fod.FirstOrderDate
+	,SUM(sol.Quantity * sol.UnitPrice) AS GrossSales
+	,SUM(sol.Quantity) AS GrossQuantity
+FROM Sales.OrderLines AS sol
+LEFT JOIN Sales.Orders AS so
+	ON sol.OrderID = so.OrderID
+LEFT JOIN FirstOrderDate AS fod
+	ON fod.Product = sol.Description
+GROUP BY 
+	DATENAME (year, so.OrderDate) 
+	,MONTH(so.OrderDate)
+	,DATENAME (month, so.OrderDate)
+	,sol.Description
+	,fod.FirstOrderDate
+HAVING SUM(sol.Quantity) >= 50
+ORDER BY 
+	DATENAME (year, so.OrderDate) ASC
+	,MONTH(so.OrderDate) ASC
+	,SUM(sol.Quantity * sol.UnitPrice) DESC
+;
+GO
+
+-----------------------------------------------------------------------
+/*
+4. Написать рекурсивный CTE sql запрос 
+и заполнить им временную таблицу и табличную переменную
+*/
+-----------------------------------------------------------------------
+
+USE WideWorldImporters;
+CREATE TABLE dbo.MyEmployees 
+( 
+	EmployeeID smallint NOT NULL, 
+	FirstName nvarchar(30) NOT NULL, 
+	LastName nvarchar(40) NOT NULL, 
+	Title nvarchar(50) NOT NULL, 
+	DeptID smallint NOT NULL, 
+	ManagerID int NULL, 
+	CONSTRAINT PK_EmployeeID PRIMARY KEY CLUSTERED (EmployeeID ASC) 
+); 
+INSERT INTO dbo.MyEmployees VALUES 
+(1, N'Ken', N'Sánchez', N'Chief Executive Officer',16,NULL) 
+,(273, N'Brian', N'Welcker', N'Vice President of Sales',3,1) 
+,(274, N'Stephen', N'Jiang', N'North American Sales Manager',3,273) 
+,(275, N'Michael', N'Blythe', N'Sales Representative',3,274) 
+,(276, N'Linda', N'Mitchell', N'Sales Representative',3,274) 
+,(285, N'Syed', N'Abbas', N'Pacific Sales Manager',3,273) 
+,(286, N'Lynn', N'Tsoflias', N'Sales Representative',3,285) 
+,(16, N'David',N'Bradley', N'Marketing Manager', 4, 273) 
+,(23, N'Mary', N'Gibson', N'Marketing Specialist', 4, 16); 
+
+-- 2do:
