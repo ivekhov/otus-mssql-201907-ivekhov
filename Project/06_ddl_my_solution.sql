a9e3b40d66ebba9d83b7a676dc7dc6d575bd0770
ivekhov <ivekhov@gmail.com>
2020-07-18 15:43:51 +0300

    updated DDL task anfd started project

--- Project/06_ddl_my_solution.sql
+++ Project/06_ddl_my_solution.sql
@@ -1,17 +1,14 @@
-'''
-Stocks db contains data on stock markets.
-'''
+-- Stocks db contains data on stock markets. #1
 CREATE DATABASE Stocks;
 GO
 
 USE Stocks;
 GO
 
+-- Creating tables with primary and foreign keys #2-3
+-- Exchanges table:
+-- contain list of world stock exchanges.
 
-'''
-Exchanges table:
-contain list of world stock exchanges.
-'''
 -- w\o constraints in create table instruction
 CREATE TABLE Exchanges(
 	Id INT not null IDENTITY
@@ -22,34 +19,26 @@ CREATE TABLE Exchanges(
 ALTER TABLE Exchanges 
 	ADD CONSTRAINT PK_Exchanges PRIMARY KEY CLUSTERED(Id);
 
+-- Indicies table
+-- Contain list of world indices. Many-to-one relation to exchanges.
 
-'''
-Indicies table
-Contain list of world indices. Many-to-one relation to exchanges.
-'''
 -- constraints inside create table script
-CREATE TABLE WorldIndiñies(
+CREATE TABLE Indicies(
 	Id int not null IDENTITY
 	, Name NVARCHAR(50)
 	, Symbol NVARCHAR(20)
 	, ExchangeId int
-	, CONSTRAINT PK_WorldIndicies PRIMARY KEY CLUSTERED(Id)
-	, CONSTRAINT FK_WorldIndicies_Exchanges FOREIGN KEY (ExchangeId) REFERENCES Exchanges (Id)
+	, CONSTRAINT PK_Indicies PRIMARY KEY CLUSTERED(Id)
+	, CONSTRAINT FK_Indicies_Exchanges FOREIGN KEY (ExchangeId) REFERENCES Exchanges (Id)
 	ON UPDATE CASCADE
 	ON DELETE CASCADE
 );
 
--- creating index
-CREATE INDEX IX_Exchange_Name ON Exchanges (Name);
-CREATE INDEX IX_WorldIndicies_Symbol ON WorldIndiñies (Symbol);
-
-'''
-Data on WorldIndices price dynamic per day
-Source: Yahoo Finance: https://finance.yahoo.com/world-indices 
-'''
-CREATE TABLE WorldIndiciesData(
+-- Data on WorldIndices price dynamic per day
+-- Source: Yahoo Finance: https://finance.yahoo.com/world-indices 
+CREATE TABLE IndiciesData(
 	Id int NOT NULL IDENTITY
-	, WorldIndexId int
+	, IndexId int
 	, LastPrice decimal(10, 2)
 	, Change decimal(10, 2)
 	, ChangePerc decimal(5, 2)
@@ -59,19 +48,14 @@ CREATE TABLE WorldIndiciesData(
 	, Last52WeekLow decimal (10, 2)
 	, Last52WeekHigh decimal (10, 2)
 	, TimeOfData datetime2
-	, CONSTRAINT PK_WorldIndiciesData PRIMARY KEY CLUSTERED(Id)
-	, CONSTRAINT FK_WorldIndiciesData_Catalog FOREIGN KEY (WorldIndexId) REFERENCES WorldIndiñies (Id)
+	, CONSTRAINT PK_IndiciesData PRIMARY KEY CLUSTERED(Id)
+	, CONSTRAINT FK_IndiciesData_Catalog FOREIGN KEY (IndexId) REFERENCES Indicies (Id)
 	ON UPDATE CASCADE
 	ON DELETE CASCADE
 );
 
--- creating index
-CREATE INDEX IX_WorldIndiciesData_ChangePerc ON WorldIndiciesData (ChangePerc);
-
-'''
-Data on ETF instruments price
-Source: Yahoo Finance: https://finance.yahoo.com/etfs
-'''
+-- Data on ETF instruments price
+-- Source: Yahoo Finance: https://finance.yahoo.com/etfs
 CREATE TABLE EtfCatalogue(
 	Id int NOT NULL IDENTITY
 	, Name NVARCHAR(50)
@@ -82,5 +66,29 @@ CREATE TABLE EtfCatalogue(
 	ON UPDATE CASCADE
 	ON DELETE CASCADE
 );
--- creating index
+
+-- creating indexes #4
+CREATE INDEX IX_Exchange_Name ON Exchanges (Name);
+CREATE INDEX IX_Indicies_Symbol ON Indicies (Symbol);
+CREATE INDEX IX_IndiciesData_ChangePerc ON IndiciesData (ChangePerc);
 CREATE INDEX IX_EtfCatalogue_Symbol ON EtfCatalogue (Symbol);
+
+
+-- adding constraint on data input #5
+ALTER TABLE Exchanges WITH CHECK
+	ADD CONSTRAINT CHK_Exchanges_Symbol
+	CHECK (LEN(Symbol) > 2);
+
+ALTER TABLE Indicies WITH CHECK
+	ADD CONSTRAINT CHK_Indicies_Symbol
+	CHECK (LEN(Symbol) > 2);
+
+ALTER TABLE IndiciesData WITH CHECK
+	ADD CONSTRAINT CHK_IndiciesData_LastPrice
+	CHECK (LastPrice >= 0);
+
+ALTER TABLE EtfCatalogue WITH CHECK
+	ADD CONSTRAINT CHK_EtfCatalogue_Symbol
+	CHECK (LEN(Symbol) > 2);
+
+----------------------------------------------------
