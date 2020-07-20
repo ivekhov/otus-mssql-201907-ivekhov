01e8493af8d6233cbe1e894767dc68201868a388
ivekhov <ivekhov@gmail.com>
2020-07-20 03:34:14 +0300

    added solution on tasks 1,2 HW08

--- HW08/8_solution.sql
+++ HW08/8_solution.sql
@@ -2,7 +2,8 @@ USE WideWorldImporters;
 
 SET STATISTICS TIME ON;
 
-
+-- task #1 
+-- creating temp table with data
 DROP TABLE IF EXISTS  #task8_temp_table_v_1;
 CREATE TABLE #task8_temp_table_v_1(
 	InvoiceID BIGINT PRIMARY KEY
@@ -10,7 +11,6 @@ CREATE TABLE #task8_temp_table_v_1(
 	, SalesDate datetime
 	, SalesSum decimal
 	, SalesMonth VARCHAR(255)
-	-- , MonthSum decimal
 );
 
 INSERT INTO #task8_temp_table_v_1
@@ -18,43 +18,129 @@ INSERT INTO #task8_temp_table_v_1
 	, CustomerName	--Sales.Invoices.CustomerID join 
 	, SalesDate		--Sales.Invoices.InvoiceDate
 	, SalesSum		--Sales.InvoiceLines.ExtendedPrice on InvoiceID
-	, SalesMonth		--Sales.Invoices.InvoiceDate
+	, SalesMonth	--Sales.Invoices.InvoiceDate
 )
 SELECT
 	si.InvoiceID
 	, sc.CustomerName
-	, si.InvoiceDate
+	, FORMAT(si.InvoiceDate, 'yyyy-MM-dd')
 	, sil.ExtendedPrice
 	, FORMAT(si.InvoiceDate, 'yyyy-MM')
 FROM Sales.Invoices AS si
 LEFT JOIN Sales.InvoiceLines AS sil
 ON (si.InvoiceID = sil.InvoiceLineID)
 LEFT JOIN Sales.Customers AS sc
-ON (si.CustomerID = sc.CustomerID);
+ON (si.CustomerID = sc.CustomerID)
+WHERE YEAR(si.InvoiceDate) >= 2015
+;
 
-SELECT * FROM #task8_temp_table_v_1;
-TRUNCATE TABLE #task8_temp_table_v_1;
+-- calculation Monthly Sales w\o window function
+SELECT
+	t.InvoiceID
+	, t.CustomerName
+	, FORMAT(t.SalesDate, 'yyyy-MM-dd')
+	, t.SalesSum
+	, m.MonthlySales
+FROM #task8_temp_table_v_1 AS t
+LEFT JOIN 
+	(
+		SELECT 
+		SalesMonth
+		, SUM(SalesSum) AS MonthlySales
+		FROM #task8_temp_table_v_1
+		GROUP BY SalesMonth
+	) AS m
+ON (t.SalesMonth = m.SalesMonth)
+ORDER BY t.SalesDate;
 
+-- Total execution time: 00:00:03.444
+-------------------------------------------------------------
+
+
+
+-- creating table variable with data
+DECLARE @task8_temp_var_v_1 table (
+	InvoiceID BIGINT PRIMARY KEY
+	, CustomerName varchar(255)
+	, SalesDate datetime
+	, SalesSum decimal
+	, SalesMonth VARCHAR(255)
+);
+
+INSERT INTO @task8_temp_var_v_1
+( 	InvoiceID		--Sales.Invoices.InvoiceID
+	, CustomerName	--Sales.Invoices.CustomerID join 
+	, SalesDate		--Sales.Invoices.InvoiceDate
+	, SalesSum		--Sales.InvoiceLines.ExtendedPrice on InvoiceID
+	, SalesMonth	--Sales.Invoices.InvoiceDate
+)
+SELECT
+	si.InvoiceID
+	, sc.CustomerName
+	, FORMAT(si.InvoiceDate, 'yyyy-MM-dd')
+	, sil.ExtendedPrice
+	, FORMAT(si.InvoiceDate, 'yyyy-MM')
+FROM Sales.Invoices AS si
+LEFT JOIN Sales.InvoiceLines AS sil
+ON (si.InvoiceID = sil.InvoiceLineID)
+LEFT JOIN Sales.Customers AS sc
+ON (si.CustomerID = sc.CustomerID)
+WHERE YEAR(si.InvoiceDate) >= 2015;
+
+-- calculation
+SELECT
+	t.InvoiceID
+	, t.CustomerName
+	, FORMAT(t.SalesDate, 'yyyy-MM-dd')
+	, t.SalesSum
+	, m.MonthlySales
+FROM @task8_temp_var_v_1 AS t
+LEFT JOIN 
+	(
+		SELECT 
+		SalesMonth
+		, SUM(SalesSum) AS MonthlySales
+		FROM @task8_temp_var_v_1
+		GROUP BY SalesMonth
+	) AS m
+ON (t.SalesMonth = m.SalesMonth)
+ORDER BY t.SalesDate;
+
+-- (with table creation and insertion) Total execution time: 00:04:03.178
+-------------------------------------------------------------
+
+
+
+
+
+-- Task #2 calculation with window function.
+SELECT
+	InvoiceID
+	, CustomerName
+	, FORMAT(SalesDate, 'yyyy-MM-dd')
+	, SalesSum
+	, SUM(SalesSum) OVER (
+		PARTITION BY SalesMonth 
+	) as MonthlySales
+FROM #task8_temp_table_v_1
+ORDER BY SalesDate;
+
+-- Total execution time: 00:00:02.681
+
+
+-- Conclusion: 
+-- 1) Window function faster on ~20% than temp tables.
+-- 2) temp variables are much slower than window functions and temp tables.
+-------------------------------------------------------------
+
+-- ToDo: 3, 4, 5, 6 + their alternatives + bonus
+
+
+
+
+
+
+-------------------------------------------------------------
+-------------------------------------------------------------
 -- how2 format datetime into yyyy--mm-dd? 
 -- FORMAT(date from table, pattern'')
-
--- ToDo: 1 create select to input in temp table
-/*  
-SELECT
-FROM 
-*/
-
--- ToDo: 2 calculate MonthSum Total 
-
-----------------------------------------
-----------------------------------------
-
-SELECT TOP(10) * FROM Sales.Customers;
-
-SELECT TOP(10) * FROM Sales.InvoiceLines; --Extended Price, InvoiceID
-
-SELECT TOP(10) * FROM Sales.Orders;
-
-SELECT TOP(10) * FROM Purchasing.PurchaseOrders;
-
-exec sp_help 'Sales.Invoices';
