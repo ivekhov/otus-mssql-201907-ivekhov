b6d6500d30526ebab65efdc397050ac56b75ba76
ivekhov <ivekhov@gmail.com>
2020-07-28 08:35:20 +0300

    Updated HW09 solution with task #1.

--- HW09_crossapply_pivot_cube/09_solution.sql
+++ HW09_crossapply_pivot_cube/09_solution.sql
@@ -13,34 +13,132 @@ GO
 дата должна иметь формат dd.mm.yyyy например 25.12.2019
 
 Например, как должны выглядеть результаты:
-ClienName           InvoiceMonth    Count 
-Peeples Valley,     01.01.2013      1
-AZ Medicine Lodge,  01.01.2013      2
-KS Gasport,         01.01.2013      3
-NY Sylvanite,       01.02.2013      4
-MT Jessie,          01.02.2013      5
-ND                  01.02.2013      6
+InvoiceMonth    Peeples Valley, AZ Medicine Lodge, KS Gasport, NY Sylvanite, MT Jessie, ND
+01.01.2013      3               1                   4           2               2 
+01.02.2013      7               3                   4           2               1
 */
 
-select top(5) * from Sales.Orders;
+-- отдельно определить поименно по идшке покупателя
+-- назвать алиасом
+-- подцепить в основной запрос по алиасу
+-- ? 
 
+
+-----------------------
+-- works but CustomerNames are hard-coded
+DECLARE @toys int;
+SET @toys = LEN('Tailspin Toys') + 3;
+WITH PivotData AS (
+SELECT
+    FORMAT(so.OrderDate, 'yyyy-MM-dd') as OrderDate
+    ,SUBSTRING(sc.CustomerName
+                    , CHARINDEX('(', sc.CustomerName)+1
+                    , LEN(sc.CustomerName) - @toys
+                    ) as ShortName
+    , so.OrderID as OrderId
+    FROM Sales.Orders as so
+    LEFT JOIN Sales.Customers as sc
+        ON so.CustomerID = sc.CustomerID
+    WHERE so.CustomerID BETWEEN 2 and 6
+)
+SELECT 
+    OrderDate
+    , [Gasport, NY]
+    , [Jessie, ND]
+    , [Medicine Lodge, KS]
+    , [Peeples Valley, AZ]
+    , [Sylvanite, MT]
+FROM PivotData
+    PIVOT(COUNT(OrderId)
+        FOR ShortName IN (
+            [Gasport, NY]
+            , [Jessie, ND]
+            , [Medicine Lodge, KS]
+            , [Peeples Valley, AZ]
+            , [Sylvanite, MT]
+        )
+) AS P
+ORDER BY OrderDate ASC;
+
+-----------------------
+
+
+-- works but too complex (with temp tables) and CustomerNames are hard-coded.
+DROP TABLE IF EXISTS #task_1;
+DECLARE @toys int;
+SET @toys = LEN('Tailspin Toys') + 3;
+CREATE TABLE #task_1 (
+    Id INT
+    , CustomerName NVARCHAR(255)
+)
+INSERT INTO #task_1(
+    Id
+    , CustomerName
+)
+select 
+    sc.CustomerID as Id
+    ,SUBSTRING(sc.CustomerName
+                    , CHARINDEX('(', sc.CustomerName)+1
+                    , LEN(sc.CustomerName) - @toys
+                    ) 
+from Sales.Customers as sc
+WHERE sc.CustomerID BETWEEN 2 AND 6;
 
 WITH PivotData AS (
-
-    SELECT 
-        so.CustomerID
-        , so.OrderDate
-        , sc.CustomerName
-        , so.OrderID
-        , SUBSTRING(sc.CustomerName
-                    , CHARINDEX('(', sc.CustomerName)+1
-                    , LEN(sc.CustomerName)
-                    -- , CHARINDEX(')', sc.CustomerName)
-                    )
+    SELECT
+        FORMAT(so.OrderDate, 'yyyy-MM-dd') as OrderDate
+        , t.CustomerName
+        , so.OrderID as OrderId
         FROM Sales.Orders as so
         LEFT JOIN Sales.Customers as sc
             ON so.CustomerID = sc.CustomerID
+        LEFT JOIN #task_1 as t
+            ON t.Id = so.CustomerID
         WHERE so.CustomerID BETWEEN 2 and 6
-
 )
+SELECT 
+    OrderDate
+    , [Gasport, NY]
+    , [Jessie, ND]
+    , [Medicine Lodge, KS]
+    , [Peeples Valley, AZ]
+    , [Sylvanite, MT]
 
+FROM PivotData
+    PIVOT(COUNT(OrderId)
+        FOR CustomerName IN (
+            [Gasport, NY]
+            , [Jessie, ND]
+            , [Medicine Lodge, KS]
+            , [Peeples Valley, AZ]
+            , [Sylvanite, MT]
+        )
+
+) AS P
+ORDER BY OrderDate ASC;
+---------------------
+
+
+
+-- works but w/o CustomerNames - inly ID.
+DECLARE @toys int;
+SET @toys = LEN('Tailspin Toys') + 3;
+WITH PivotData AS (
+SELECT
+    FORMAT(so.OrderDate, 'yyyy-MM-dd') as OrderDate
+    , sc.CustomerID as ShortName
+    , so.OrderID as OrderId
+    FROM Sales.Orders as so
+    LEFT JOIN Sales.Customers as sc
+        ON so.CustomerID = sc.CustomerID
+    WHERE so.CustomerID BETWEEN 2 and 6
+)
+SELECT OrderDate, [2], [3], [4], [5], [6]
+FROM PivotData
+    PIVOT(COUNT(OrderId)
+        FOR ShortName IN ([2], [3], [4], [5], [6])
+) AS P
+ORDER BY OrderDate ASC;
+---------------------
+
+-- ToDo: put CustomerNames from script, not hard-coding.
