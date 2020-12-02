e2a701e564fe206888b141f30715ecdf82edf0be
ivekhov <ivekhov@gmail.com>
2020-12-02 18:55:55 +0300

    Added solution in other goods testing.

--- HW09_crossapply_pivot_cube/09_solution.sql
+++ HW09_crossapply_pivot_cube/09_solution.sql
@@ -5,7 +5,7 @@ GO
 -- count of orders
 /*
 1. Требуется написать запрос, который в результате своего выполнения формирует таблицу следующего вида:
-Название клиента МесяцГод Количество покупок
+Название клиента  - МесяцГод - Количество покупок
 
 Клиентов взять с ID 2-6, это все подразделение Tailspin Toys
 имя клиента нужно поменять так чтобы осталось только уточнение
@@ -18,185 +18,61 @@ InvoiceMonth    Peeples Valley, AZ Medicine Lodge, KS Gasport, NY Sylvanite, MT 
 01.02.2013      7               3                   4           2               1
 */
 
--- отдельно определить поименно по идшке покупателя
--- назвать алиасом
--- подцепить в основной запрос по алиасу
--- ? 
 
-
------------------------
--- works but CustomerNames are hard-coded
-DECLARE @toys int;
-SET @toys = LEN('Tailspin Toys') + 3;
 WITH PivotData AS (
-SELECT
-    FORMAT(so.OrderDate, 'yyyy-MM-dd') as OrderDate
-    ,SUBSTRING(sc.CustomerName
-                    , CHARINDEX('(', sc.CustomerName)+1
-                    , LEN(sc.CustomerName) - @toys
-                    ) as ShortName
-    , so.OrderID as OrderId
-    FROM Sales.Orders as so
-    LEFT JOIN Sales.Customers as sc
-        ON so.CustomerID = sc.CustomerID
-    WHERE so.CustomerID BETWEEN 2 and 6
+    SELECT InvoiceMonth
+         , CustomerName
+         , OrderId
+    FROM (SELECT
+                FORMAT(o.OrderDate, 'dd.MM.yyyy') as InvoiceMonth
+               , substring(c.CustomerName,
+                            CHARINDEX('(', c.CustomerName) + 1,
+                            CHARINDEX(')', c.CustomerName) - CHARINDEX('(', c.CustomerName) - 1
+                ) as CustomerName
+               , o.OrderID  as OrderId
+          FROM Sales.Orders AS o
+                   LEFT JOIN Sales.Customers AS c
+                             ON c.CustomerID = o.CustomerID
+          WHERE o.CustomerID in (2, 3, 4, 5, 6)
+         ) as [IMCIOI]
 )
-SELECT 
-    OrderDate
-    , [Gasport, NY]
-    , [Jessie, ND]
-    , [Medicine Lodge, KS]
-    , [Peeples Valley, AZ]
-    , [Sylvanite, MT]
+SELECT InvoiceMonth,
+--        'Sylvanite, MT', 'Peeples Valley, AZ' , 'Medicine Lodge, KS', 'Gasport, NY', 'Jessie, ND'
+
+       ['Sylvanite, MT'], ['Peeples Valley, AZ'] , ['Medicine Lodge, KS'], ['Gasport, NY'], ['Jessie, ND']
 FROM PivotData
-    PIVOT(COUNT(OrderId)
-        FOR ShortName IN (
-            [Gasport, NY]
-            , [Jessie, ND]
-            , [Medicine Lodge, KS]
-            , [Peeples Valley, AZ]
-            , [Sylvanite, MT]
+    PIVOT (
+        COUNT(OrderId)
+        FOR CustomerName in (['Sylvanite, MT'], ['Peeples Valley, AZ'] , ['Medicine Lodge, KS'], ['Gasport, NY'], ['Jessie, ND'])
+
+--          FOR CustomerName in ('Sylvanite, MT', 'Peeples Valley, AZ' , 'Medicine Lodge, KS', 'Gasport, NY', 'Jessie, ND')
+
         )
-) AS P
-ORDER BY OrderDate ASC;
-
------------------------
+    AS P;
 
 
--- works but too complex (with temp tables) and CustomerNames are hard-coded.
-DROP TABLE IF EXISTS #task_1;
-DECLARE @toys int;
-SET @toys = LEN('Tailspin Toys') + 3;
-CREATE TABLE #task_1 (
-    Id INT
-    , CustomerName NVARCHAR(255)
-)
-INSERT INTO #task_1(
-    Id
-    , CustomerName
-)
-select 
-    sc.CustomerID as Id
-    ,SUBSTRING(sc.CustomerName
-                    , CHARINDEX('(', sc.CustomerName)+1
-                    , LEN(sc.CustomerName) - @toys
-                    ) 
-from Sales.Customers as sc
-WHERE sc.CustomerID BETWEEN 2 AND 6;
 
-WITH PivotData AS (
+
+
+
+
+
+
     SELECT
-        FORMAT(so.OrderDate, 'yyyy-MM-dd') as OrderDate
-        , t.CustomerName
-        , so.OrderID as OrderId
-        FROM Sales.Orders as so
-        LEFT JOIN Sales.Customers as sc
-            ON so.CustomerID = sc.CustomerID
-        LEFT JOIN #task_1 as t
-            ON t.Id = so.CustomerID
-        WHERE so.CustomerID BETWEEN 2 and 6
-)
-SELECT 
-    OrderDate
-    , [Gasport, NY]
-    , [Jessie, ND]
-    , [Medicine Lodge, KS]
-    , [Peeples Valley, AZ]
-    , [Sylvanite, MT]
-
-FROM PivotData
-    PIVOT(COUNT(OrderId)
-        FOR CustomerName IN (
-            [Gasport, NY]
-            , [Jessie, ND]
-            , [Medicine Lodge, KS]
-            , [Peeples Valley, AZ]
-            , [Sylvanite, MT]
+    FROM (SELECT
+                FORMAT(o.OrderDate, 'MM.yyyy') as InvoiceMonth
+               , substring(c.CustomerName,
+                            CHARINDEX('(', c.CustomerName) + 1,
+                            CHARINDEX(')', c.CustomerName) - CHARINDEX('(', c.CustomerName) - 1
+                    ) as CustomerName
+               , o.OrderID as CountOrders
+          FROM Sales.Orders AS o
+                   LEFT JOIN Sales.Customers AS c
+                             ON c.CustomerID = o.CustomerID
+          WHERE o.CustomerID in (2, 3, 4, 5, 6)
+         ) as s
+    PIVOT (
+        COUNT(CountOrders)
+        FOR CustomerName in (['Sylvanite, MT'], ['Peeples Valley, AZ'] , ['Medicine Lodge, KS'], ['Gasport, NY'], ['Jessie, ND'])
         )
-
-) AS P
-ORDER BY OrderDate ASC;
----------------------
-
-
-
--- works but w/o CustomerNames - inly ID.
-DECLARE @toys int;
-SET @toys = LEN('Tailspin Toys') + 3;
-WITH PivotData AS (
-SELECT
-    FORMAT(so.OrderDate, 'yyyy-MM-dd') as OrderDate
-    , sc.CustomerID as ShortName
-    , so.OrderID as OrderId
-    FROM Sales.Orders as so
-    LEFT JOIN Sales.Customers as sc
-        ON so.CustomerID = sc.CustomerID
-    WHERE so.CustomerID BETWEEN 2 and 6
-)
-SELECT OrderDate, [2], [3], [4], [5], [6]
-FROM PivotData
-    PIVOT(COUNT(OrderId)
-        FOR ShortName IN ([2], [3], [4], [5], [6])
-) AS P
-ORDER BY OrderDate ASC;
----------------------
-
--- ToDo: put CustomerNames from script, not hard-coding.
------------------------------------------------------
-
-
-/*
-2. Для всех клиентов с именем, в котором есть Tailspin Toys
-вывести все адреса, которые есть в таблице, в одной колонке
-
-Пример результатов:
-CustomerName                    AddressLine
-Tailspin Toys (Head Office)     Shop 38
-Tailspin Toys (Head Office)     1877 Mittal Road
-Tailspin Toys (Head Office)     PO Box 8975
-Tailspin Toys (Head Office)     Ribeiroville
-*/
-
--- steps: 
--- create sample of rows for condition
--- put cross apply condition
-
-
--- possible solution (?) new information on task needed. 
-SELECT 
-    CustomerName
-    , a.*
-FROM Sales.Customers as sc1
-    CROSS APPLY (
-        SELECT DISTINCT DeliveryAddressLine1
-        FROM Sales.Customers as sc2
-        -- WHERE sc1.CustomerID = sc2.CustomerID
-    ) AS a
--- WHERE CustomerName LIKE '%Tailspin Toys%'
-WHERE CustomerName = 'Tailspin Toys (Head Office)'
-ORDER BY CustomerName;
-
-
-
-SELECT DeliveryAddressLine1, DeliveryAddressLine2
-FROM Sales.Customers
-where DeliveryAddressLine2 like '%Ribeiroville%' 
-    or DeliveryAddressLine2 like '%PO Box 8975%'
-    or DeliveryAddressLine2 like '%1877 Mittal Road%'
-    or DeliveryAddressLine1  like '%Ribeiroville%' 
-    or DeliveryAddressLine1 like '%PO Box 8975%'
-    or DeliveryAddressLine1 like '%1877 Mittal Road%'
-;
--- result is one row :Shop 38	1877 Mittal Road
-
-
-
-----------------------
--- wrong solution.
-SELECT 
-    CustomerName
-    , DeliveryAddressLine1
-FROM Sales.Customers
-WHERE CustomerName LIKE '%Tailspin Toys%'
-ORDER BY CustomerName;
-----------------------
+    AS P;
---
+++ other/goods.sql
@@ -0,0 +1,58 @@
+/***
+Необходимо написать SQL-запрос
+  (стандарт ANSI или любой диалект на выбор),
+  выводящий
+  среднюю стоимость покупки клиентов
+  из центрального региона ("Central"),
+  совершившим первую покупку в январе 2018 года.
+  Результаты предоставить в разбивке по городам.
+***/
+
+USE goods;
+WITH ClientOrders AS (
+    SELECT O.CustomerID
+            , O.OrderID
+            , MONTH(CONVERT(datetime2, CAST(O.OrderDate AS NVARCHAR), 126)) as Month
+            , YEAR(CONVERT(datetime2, CAST(O.OrderDate AS NVARCHAR), 126)) as Year
+            , C.CityID
+            , CR.Region
+            , OL.SKU
+            , (OL.Quantity*OL.Price) AS SkuPrice
+            , RANK() over (
+                    PARTITION BY O.CustomerID
+                    ORDER BY MONTH(CONVERT(datetime2, CAST(O.OrderDate AS NVARCHAR), 126)) ASC
+                )  AS OrderNumPerClient
+    FROM test.Orders AS O
+    LEFT JOIN test.Order_List OL on O.OrderID = OL.OrderID
+    LEFT JOIN test.Customers C ON C.CustomerID = O.CustomerID
+    LEFT JOIN test.City_Region CR on CR.CityID = CAST(C.CityID AS int)
+    WHERE O.OrderState = 'Fulfilled' AND C.CityID != 'NULL' AND CR.Region = 'Central'
+), SelectedCustomers AS (
+    SELECT DISTINCT CO.CustomerID
+    FROM ClientOrders AS CO
+    WHERE CO.OrderNumPerClient = 1
+        AND CO.Month = 1
+        AND CO.Year = 2018
+)
+, CityOrders AS (
+SELECT
+        CO.CityID
+        , CO.OrderID
+        , SUM(CO.SkuPrice) as OrderSum
+FROM ClientOrders AS CO
+    RIGHT JOIN SelectedCustomers AS SC ON SC.CustomerID = CO.CustomerID
+
+GROUP BY CO.CityID, CO.OrderID)
+
+-- select top(100) *
+-- from ClientOrders
+
+SELECT COD.CityID, AVG(OrderSum)
+FROM CityOrders as COD
+GROUP BY CityID
+
+-- select top(10) *
+-- from CityOrders
+-- where OrderID = 224660
+
+;
